41fb9cf4fd35b8922fa8c7a5dd8d14bb8fb586c8
Byron Hinebaugh <bhinebaugh@gmail.com>
2017-05-25 22:57:38 -0400

    dynamically list ref and narrative pages in roots section

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+import { NarrativeService } from './narrative.service';
+import { ReferenceService } from './reference.service';
+
 @Component({
   selector: 'landing',
-  templateUrl: './templates/landing.html'
+  templateUrl: './templates/landing.html',
+  providers: [ NarrativeService, ReferenceService ]
 })
 export class LandingComponent {
   // these values are changed inline in the template by event listeners on the buttons
   // which triggers ngClass to add classes to the respective elements
   episodesVisible: false;
   rootLinksVisible: false;
+  narrativePages: Array<any>;
+  referencePages: Array<any>;
+  
+  constructor( narrativeService:NarrativeService, referenceService:ReferenceService ) {
+    this.narrativePages = narrativeService.getNarrativePages();
+    this.referencePages = referenceService.getReferencePages();
+  }
 }
