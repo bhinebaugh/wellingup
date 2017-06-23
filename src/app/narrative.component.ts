c898cf83838c81fe01f57b557b447944e6abacc1
Byron Hinebaugh <bhinebaugh@gmail.com>
2017-06-23 15:52:59 -0400

    fixed rendering of html in link titles

--- src/app/narrative.component.ts
+++ src/app/narrative.component.ts
@@ -9,7 +9,7 @@ import { slideAnimation } from './animations';
   selector: 'narrative',
   template: `
   <ul>
-    <li *ngFor="let page of pages"><a routerLink={{page.id}}>{{ page.title.rendered }}</a></li>
+    <li *ngFor="let page of pages"><a routerLink={{page.id}} [innerHtml]="page.title.rendered"></a></li>
   </ul>
   `,
   providers: [ NarrativeService ]
