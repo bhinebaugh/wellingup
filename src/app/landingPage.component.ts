1eb4ebf34494a13040c15cf81e6560f7780fa4c6
Peter Carlson <Peter Carlson>
2017-08-03 13:25:07 -0400

    Styled header

---
+++ src/app/landingPage.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {EpisodeComponent} from './episode.component';
+
+@Component({
+	selector: 'landing-page',
+	template: `
+	<div>
+		<h1>WHAT'S UP NERD</h1>
+	</div>
+	`
+})
+export class LandingPage{};
