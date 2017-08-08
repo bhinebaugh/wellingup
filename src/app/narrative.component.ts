790f333fc4ee604123e3fc5545bf7e4866ae6dfb
Byron Hinebaugh <bhinebaugh@gmail.com>
2017-08-08 15:38:48 -0400

    interim refactor

---
+++ src/app/narrative.component.ts
@@ -0,0 +1,27 @@
+import { Component, HostBinding } from '@angular/core';
+
+import { Page } from './page';
+import { ContentService } from './content.service';
+import { slideAnimation } from './animations';
+
+@Component({
+  animations: [ slideAnimation ],
+  selector: 'narrative',
+  template: `
+  <ul>
+    <li *ngFor="let page of pages"><a routerLink={{page.id}} [innerHtml]="page.title.rendered"></a></li>
+  </ul>
+  `,
+  providers: [ ContentService ]
+})
+export class PageComponent {
+  @HostBinding('@routeAnimation') routeAnimation = true;
+	@HostBinding('style.display')   display = 'block';
+	@HostBinding('style.position')  position = 'absolute';
+
+	pages: Array<Page>;
+
+	constructor( contentService:ContentService ) {
+		contentService.getNarrativePages().then(pages => this.pages = pages);
+	}
+}
