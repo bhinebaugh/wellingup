50636b63ecbe20e307e6cc1043aed1c303233b10
Byron Hinebaugh <bhinebaugh@gmail.com>
2017-08-09 13:44:31 -0400

    transitioning to subscribinb route observable onInit to enable category component reuse

--- src/app/category.component.ts
+++ src/app/category.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+
 import { Category } from './category';
 import { Page } from './page';
 import { ContentService } from './content.service';
-import { ActivatedRoute, Params } from '@angular/router';
 import { slideAnimation } from './animations';
 
 @Component({
@@ -27,13 +30,31 @@ export class CategoryComponent implements OnInit {
         private contentService : ContentService,
         private route : ActivatedRoute
     ){
-        let id = +this.route.snapshot.params['id'];
-        contentService.getPagesForCategory(id).then(returnedPages => this.pages = returnedPages );
+        // let id = +this.route.snapshot.params['id'];
+        // contentService.getPagesForCategory(id).then(returnedPages => this.pages = returnedPages );
         // get Category name, and breadcrumb also
-        this.contentService.getCategory(id).then(returnedCat => this.category = returnedCat)
+        // this.contentService.getCategory(id).then(returnedCat => this.category = returnedCat)
     }
 
     ngOnInit(): void {
+        // no errors but never assigns this.pages
+        // this.route.paramMap.map(
+        //     (params : ParamMap) => {
+        //         console.log('route params changed');
+        //         this.contentService.getPagesForCategory(+params.get('id'))
+        //         .then( (returnedPages: Page[]) => this.pages = returnedPages)
+        //     }
+        // );
+        
+        // maybe? diff between params and paramMap? abandoning.
+        // this.route.params.map(
+        //     params => this.contentService.getPagesForCategory(1)
+        // )
 
+        // hewing close to tutorial
+        // need to add a method to service that returns an Observable
+        // this.route.paramMap.switchMap(
+        //     (params : ParamMap) => this.contentService.getNarrativePages$()
+        // )
     }
 }
