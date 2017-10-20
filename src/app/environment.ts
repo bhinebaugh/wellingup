947b2be6b6e9f3b59ab9ffa5fbf2d2e2c20bc86c
Peter Carlson <pgrcarlson@gmail.com>
2017-10-20 12:57:14 -0400

    Changed address reference in environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    wordpressUrl: 'https://45.76.2.113/wellingup/wp-json/wp/v2',
+    wordpressUrl: 'https://wellingup.net/wellingup/wp-json/wp/v2',
     narrativeCategory: 1,
     referenceCategory: 2,
     synopsisCategory: 8,
